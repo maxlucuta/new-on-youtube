3fd20184772b77d39db18b830e0344af32f54309
Maximilian Lucuta <113853857+maxlucuta@users.noreply.github.com>
2023-01-21 22:39:33 +0000

    youtube transcript function implemented

--- server/website/yt_api.py
+++ server/website/yt_api.py
@@ -1,11 +1,24 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
-transcript = YouTubeTranscriptApi.get_transcript("HulPgd-QgJk")
 
-transcript_list = ""
+def generate_transcripts(key):
+    """ Calls youtube_transcript api to return a transcript 
+        for a specific video.
 
-for i in transcript:
-    if(i['text'] != '[Music]'):
-        transcript_list += i['text']
+        Args:
+            key: string -> unique key for video.
 
-print(transcript_list)
+        Returns:
+            transcript: string -> transcript for the video.
+    """
+    raw_transcript = YouTubeTranscriptApi.get_transcript(key)
+    final_transcript = []
+
+    for text in raw_transcript:
+        word = text.get('text')
+        if not word or word == '[Music]':
+            continue
+        final_transcript.append(word)
+    
+    return " ".join(final_transcript)
+        
