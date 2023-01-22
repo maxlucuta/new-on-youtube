4b23b7b27d22663cd410b6565ded6906d31ffe25
Maximilian Lucuta <113853857+maxlucuta@users.noreply.github.com>
2023-01-22 18:58:59 +0000

    added pytest youtube api test

---
+++ server/tests/unit/test_youtube.py
@@ -0,0 +1,20 @@
+import pytest 
+from website.utilities import youtube as yt
+
+def test_youtube_api():
+
+    topic = "football"
+    count = 1
+
+    response = yt.get_most_popular_video_transcripts_by_topic(topic, count)
+
+    for data in response:
+        key = data.get('keyword')
+        video = data.get('video_title')
+        channel = data.get('channel_name')
+        summary = data.get('summary')
+
+        assert key and video and channel and summary
+        assert len(data) == 4
+    
+    assert len(response) == count
