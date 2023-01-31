44eef9f2a79015825fe49dfaf9ed985d57807af9
David Bull <18296145+david-w-bull@users.noreply.github.com>
2023-01-31 22:40:42 +0000

    Added tests for video endpoint functions

--- server/tests/unit/test_youtube.py
+++ server/tests/unit/test_youtube.py
@@ -18,3 +18,27 @@ def test_youtube_api():
         assert len(data) == 4
     
     assert len(response) == count
+
+def test_youtube_video_endpoint_by_id():
+    
+    video_ids = ["DbqPrMTrQHc", "YOts1Crp21A", "BYVZh5kqaFg"]
+    response = get_video_information_by_id(video_ids)
+
+    for data in response:
+        ## Test mandatory response data is not None
+        assert data.get('video_id')
+        assert data.get('video_name')
+
+    assert len(response) == len(video_ids)
+
+def test_youtube_video_endpoint_charts():
+    
+    count = 3
+    response = get_charting_video_information(count)
+
+    for data in response:
+        ## Test mandatory response data is not None
+        assert data.get('video_id')
+        assert data.get('video_name')
+
+    assert len(response) == count
