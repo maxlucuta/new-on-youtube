ff7406a030eae5f6b784e0992664d909414f6d5b
anu16 <albert.ugwudike16@imperial.ac.uk>
2023-02-03 17:01:15 +0000

    trying reducing to one worker

--- server/gunicorn_config.py
+++ server/gunicorn_config.py
@@ -4,7 +4,8 @@ import multiprocessing
 port = os.environ.get("PORT", 5000)
 
 bind = f"0.0.0.0:{port}"
-workers = multiprocessing.cpu_count() * 2 + 1
+# workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
 timeout = 500
 capture_output = True
 enable_stdio_inheritance = True
