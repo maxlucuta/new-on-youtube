86605adb740e6949bd98e3a741db3042edcc2ba7
David Bull <18296145+david-w-bull@users.noreply.github.com>
2023-02-04 13:47:46 +0000

    Changes to YouTube API call to allow more than 50 results per call.
    Full information can now returned by get_most_popular_video_transcripts_by_topic

--- server/website/utilities/youtube.py
+++ server/website/utilities/youtube.py
@@ -8,47 +8,87 @@ API_KEY = "AIzaSyCEYn_Co51eJlG5sCbzVbPQ9HQn-RHxRms"
 youtube = googleapi.build("youtube", "v3", developerKey = API_KEY)
 
 
-def get_most_popular_video_transcripts_by_topic(topic, result_num, fast_summarizer=False):
-    """ Calls youtube data api to fetch metadata for videos of
-        the specific topic, returning processed data to be 
-        stored in models.py database.
+def get_most_popular_video_transcripts_by_topic(topic, videos_requested, fast_summarizer=False, include_summary=True, pause_length=1):
+    """Calls youtube data api to fetch metadata for videos of
+    the specific topic, returning processed data to be stored in the database.
+    A maximum of 50 videos can be fetched from the API per call.
+    Requests for a larger number of videos will make multiple calls to the API.
 
-        Args:
-            topic: string -> video topic
-            result_num: int -> number of videos to query
+    Args:
+        topic: string -> video topic
+        result_num: int -> number of videos to query
+        fast_summarizer: bool -> will use a summarizer model when True, GPT-3 API call when False
+        include_summary: bool -> when False only video information will be fetched, no summary will be generated
+        pause_length: int -> the time in seconds to wait between calls to GPT-3 API.
 
-        Returns:
-            transcript: [{dict}] in the format found in models.py
+    Returns:
+        results: [{dict}] in the format found in models.py
     """
-    global youtube
-    formatted_request_response = []
-    instruction = "Please summarise this transcript for me in a few sentences: "
+    results = []
 
+    ## Note max_requests of 50 is set by the YouTube API
+    max_requests = 50
+    
+    if videos_requested > max_requests:
+        videos_to_request = max_requests
+    else:
+        videos_to_request = videos_requested
+        
+    remaining_videos = videos_requested - max_requests
+    
+    current_request = get_search_endpoint_ids(topic, videos_to_request, "")
+    video_ids = current_request['video_ids']
+    next_page = current_request.get('next_page')
+    
+    while(remaining_videos > 0):
+        if remaining_videos > max_requests:
+            videos_to_request = max_requests
+        else:
+            videos_to_request = remaining_videos
+            
+        current_request = get_search_endpoint_ids(topic, videos_to_request, next_page)
+        video_ids.extend(current_request['video_ids'])
+        next_page = current_request.get('next_page')
+        remaining_videos -= max_requests
+    
+    ## Check fetched videos are unique as API pagination can be inconsistent
+    video_ids = list(set(video_ids))
+    
+    ## Retrieve the video information by id in groups of max size 50
+    while video_ids:
+        i = len(video_ids) - max_requests
+        results.extend(get_video_information_by_id(video_ids[i:], fast_summarizer, include_summary, pause_length))
+        del video_ids[i:]
+    
+    return results
+
+def get_search_endpoint_ids(topic, num_videos, page_token):
+    """Gets video IDs for a given topic from the YouTube Videos:List API endpoint.
+    Accepts a 'page_token' parameter to allow for paginated results
+    
+    Args:
+        topic: string -> video topic
+        num_videos: int -> number of videos to query (maximum 50)
+        page_token: string -> token for YouTube API pagination. 
+            An empty string returns the first page. 
+
+    Returns:
+        results: [{dict}] in the format found in models.py
+    
+    """
     request = youtube.search().list(part = "snippet", q = topic,
-                                    maxResults = result_num, 
-                                    order = "viewCount", type = "video",
-                                    regionCode = "gb", safeSearch = "moderate",
+                                    maxResults = num_videos, 
+                                    order = "viewCount", pageToken = page_token,
+                                    type = "video", regionCode = "gb", 
+                                    safeSearch = "moderate", 
                                     videoDuration = "short",
                                     videoCaption = "closedCaption")
-
+    
     response = request.execute()
-
-    for item in response['items']:
-        video_title = item['snippet']['title']
-        video_id = item['id']['videoId']
-        channel_name = item['snippet']['channelTitle']
-
-        transcript = instruction + generate_transcript(video_id) + "\n\nTl;dr"
-        summary = summarize_yt_script_with_gpt3(transcript)
-        formatted_request_response.append({'keyword' : topic, 
-                                           'video_title' : video_title,
-                                           'channel_name' : channel_name,
-                                           'summary' : summary})
-        
-        if not fast_summarizer: time.sleep(5)
-
-    return formatted_request_response
-
+    
+    video_ids = [item['id']['videoId'] for item in response['items']]
+    
+    return {'video_ids': video_ids, 'next_page': response['nextPageToken']}
 
 def generate_transcript(key):
     """ Calls youtube_transcript api to return a transcript 
@@ -74,7 +114,7 @@ def generate_transcript(key):
     except YouTubeTranscriptApi.NoTranscriptFound:
         return ""
 
-def parse_video_endpoint_response(response):
+def parse_video_endpoint_response(response_item):
     """ Parses the 'items' list in the response returned by the YouTube API 
     'Videos:list' endpoint, including treating missing values. This is used as 
     a helper function by multiple functions that call this endpoint. 
@@ -86,47 +126,50 @@ def parse_video_endpoint_response(response):
         parsed_item_details: [{dict}] -> A list of flattened dicts 
         containing information for each video item. 
     """
-    parsed_item_details = []
+    video_id = response_item["id"]
+        
+    video_overview = response_item.get("snippet", {})
+    content_details = response_item.get("contentDetails", {})
+    topics = response_item.get("topicDetails", {})
+    statistics = response_item.get("statistics", {})
     
-    for response_item in response["items"]:
-        video_id = response_item["id"]
+    item_details = {'video_id': video_id,
+                    'video_name' : video_overview.get('title'),
+                    'channel_id' : video_overview.get('channelId'),
+                    'channel_name' : video_overview.get('channelTitle'),
+                    'video_description' : video_overview.get('description'),
+                    'video_tags' : video_overview.get('tags', []), 
+                    'published_at' : video_overview.get('publishedAt'),
+                    'video_topics' : topics.get('topicCategories', []),
+                    'views' : statistics.get('viewCount'),
+                    'likes' : statistics.get('likeCount'),
+                    'quality' : content_details.get('definition'),
+                    'duration' : content_details.get('duration')
+                    }
+
         
-        video_overview = response_item.get("snippet", {})
-        content_details = response_item.get("contentDetails", {})
-        topics = response_item.get("topicDetails", {})
-        statistics = response_item.get("statistics", {})
-        
-        item_details = {'video_id': video_id,
-                        'video_name' : video_overview.get('title'),
-                        'channel_id' : video_overview.get('channelId'),
-                        'channel_name' : video_overview.get('channelTitle'),
-                        'video_description' : video_overview.get('description'),
-                        'video_tags' : video_overview.get('tags', []),
-                        'published_at' : video_overview.get('publishedAt'),
-                        'video_topics' : topics.get('topicCategories', []),
-                        'views' : statistics.get('viewCount'),
-                        'likes' : statistics.get('likeCount'),
-                        'quality' : content_details.get('definition'),
-                        'duration' : content_details.get('duration')
-                        }
-        parsed_item_details.append(item_details)
-        
-    return parsed_item_details
+    return item_details
     
 
-def get_video_information_by_id(video_ids):
+def get_video_information_by_id(video_ids, fast_summarizer, include_summary, pause_length):
     """ Calls the YouTube API 'Videos:list' endpoint to retreive information 
     on a specified list of videos, returning the processed information in a 
     format to be consumed by database.py.
     
     Args:
         video_ids: list -> a list of YouTube video ids (maximum 50).
+        fast_summarizer: bool -> will use a summarizer model when True, GPT-3 API call when False
+        include_summary: bool -> when False only video information will be fetched, no summary will be generated
+        pause_length: int -> the time in seconds to wait between calls to GPT-3 API.
         
     Returns:
         [{dict}] -> A list of flattened dicts containing information for 
-        each video item in the response.
+            each video item in the response.
     """
     global youtube
+    
+    full_video_information = []
+    instruction = "Please summarise this transcript for me in a few sentences: "
     
     information_sections = ["snippet", "contentDetails", "statistics",
                             "topicDetails"]
@@ -136,9 +179,33 @@ def get_video_information_by_id(video_ids):
                                     id = ",".join(video_ids), 
                                     regionCode = "gb",
                                     maxResults = number_videos)
+    
     response = request.execute()
+    
+    for item in response['items']:
+        video_information = parse_video_endpoint_response(item)
         
-    return parse_video_endpoint_response(response)
+        if include_summary:
+            transcript = generate_transcript(video_information['video_id'])
+            
+            ## Errors in transcript generation will return an empty string
+            ## In these cases no video information record will be created
+            if transcript:
+                gpt_prompt = instruction + transcript + "\n\nTl;dr"
+                video_information['summary'] = summarize_yt_script_with_gpt3(gpt_prompt)
+                
+                ## Remove common prefixes on GPT-3 outputs
+                video_information['summary'] = video_information['summary'].strip(" :-")
+                
+                
+                if not fast_summarizer:
+                    time.sleep(pause_length)
+        else:
+            video_information['summary'] = ""
+            
+        full_video_information.append(video_information)
+            
+    return full_video_information
 
 
 def get_charting_video_information(result_num):
