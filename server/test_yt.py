c48f965b09d73186af581183162e165cbf7d7da3
Alex Arzt <80107814+aarzt21@users.noreply.github.com>
2023-02-04 20:03:55 +0000

    extended function that inserts into DB

---
+++ server/test_yt.py
@@ -0,0 +1,11 @@
+
+from website.utilities.youtube import get_most_popular_video_transcripts_by_topic
+from website.utilities.database import establish_connection, insert_into_DB
+
+conn = establish_connection()
+res = get_most_popular_video_transcripts_by_topic("marble", 1)
+
+
+for i in res: 
+    ins = insert_into_DB(i, conn)
+    print(ins)
