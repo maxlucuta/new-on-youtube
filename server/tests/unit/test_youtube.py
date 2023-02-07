d10a0b77c99f258d432c6066852afdf7e18c463c
David Bull <18296145+david-w-bull@users.noreply.github.com>
2023-02-07 21:52:28 +0000

    Removed test for video endpoint function.
    This is now used as a helper and therefore tested through test_youtube_api

--- server/tests/unit/test_youtube.py
+++ server/tests/unit/test_youtube.py
@@ -1,6 +1,5 @@
 from website.utilities import youtube as yt
 
-
 def test_youtube_api():
     """Test YouTube API methods are correctly
        processing requests.
@@ -17,19 +16,3 @@ def test_youtube_api():
         assert key and video and channel and summary
 
     assert len(response) == count
-
-
-def test_youtube_video_endpoint_by_id():
-
-    video_ids = ["DbqPrMTrQHc", "YOts1Crp21A", "BYVZh5kqaFg"]
-    response = yt.get_video_information_by_id(video_ids,
-                                              fast_summarizer=False,
-                                              include_summary=False,
-                                              pause_length=1)
-
-    for data in response:
-        # Test mandatory response data is not None
-        assert data.get('video_id')
-        assert data.get('video_name')
-
-    assert len(response) == len(video_ids)
