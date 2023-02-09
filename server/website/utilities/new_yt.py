374802136bcad71b52a59f462186c901edddc8a7
Maximilian Lucuta <113853857+maxlucuta@users.noreply.github.com>
2023-02-09 19:03:14 +0000

    lol.v2

--- server/website/utilities/new_yt.py
+++ server/website/utilities/new_yt.py
@@ -109,12 +109,10 @@ class YoutubeParser:
 
 
     def _generate_summaries(self, rate):
-        count = 0
         for data in self.response:
             try:
                 transcript = data['transcript']
                 data["summary"] = self.summarise(transcript, rate)
-                count += 1
                 print("Got " + str(count) + " video(s)!")
             except (RateLimitError, ServiceUnavailableError): 
                 continue
