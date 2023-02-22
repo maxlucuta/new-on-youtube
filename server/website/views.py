9fc9ce0365550c116e5bb31b77a07526d88b2579
AlbertUgwudike <52350655+AlbertUgwudike@users.noreply.github.com>
2023-02-22 16:08:27 +0000

    run tests run_deploy

--- server/website/views.py
+++ server/website/views.py
@@ -3,7 +3,7 @@ from flask import Blueprint, send_from_directory
 
 # changed static folder path
 views_blueprint = Blueprint('views_blueprint', __name__,
-                            static_folder='./static',
+                            static_folder='../static',
                             static_url_path='/') if \
     os.environ.get('IN_DOCKER_CONTAINER', False) else \
     Blueprint('views_blueprint', __name__, static_folder='../../client/build')
@@ -17,3 +17,7 @@ def landing_page(path):
         return send_from_directory(views_blueprint.static_folder, path)
     else:
         return send_from_directory(views_blueprint.static_folder, 'index.html')
+
+# @views_blueprint.errorhandler(404)
+# def not_found(e):
+#     return app.send_static_file('index.html')
