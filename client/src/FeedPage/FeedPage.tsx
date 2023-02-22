0501c5b2cc68d8ba604cba62ac7924da4ccda341
David Bull <david.w.bull@gmail.com>
2023-02-22 23:47:13 +0000

    Removed unused styled components (moved to Result.tsx)

--- client/src/FeedPage/FeedPage.tsx
+++ client/src/FeedPage/FeedPage.tsx
@@ -76,29 +76,3 @@ const FeedSelector = styled.div<{ selected: boolean }>`
         cursor: pointer;
     }
 `;
-
-const Result = styled.a`
-    display: flex;
-    text-decoration: none;
-    color: black;
-    align-items: center;
-    padding: 10px;
-    background-color: #e1e1e1;
-    border-radius: 10px;
-    margin: 20px 0;
-    &:hover {
-        cursor: pointer;
-        background-color: #fff1ac;
-    }
-`;
-
-const Img = styled.img`
-    border-radius: 10px;
-    width: 15%;
-`;
-
-const Description = styled.div`
-    text-align: center;
-    width: 85%;
-    font-size: 25px;
-`;
