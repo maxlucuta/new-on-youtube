4e7a390ac2e7431e7e42a83a357435e0c595879d
AlbertUgwudike <52350655+AlbertUgwudike@users.noreply.github.com>
2023-02-25 22:14:10 +0000

    Added nginx configuration file and updated docker image

--- server/website/views.py
+++ server/website/views.py
@@ -1,5 +1,5 @@
 import os
-from flask import Blueprint, send_from_directory
+from flask import Blueprint, send_from_directory, render_template, send_file
 
 
 views_blueprint = Blueprint('views_blueprint', __name__,
@@ -12,8 +12,12 @@ views_blueprint = Blueprint('views_blueprint', __name__,
 @views_blueprint.route('/', defaults={'path': ''})
 @views_blueprint.route('/<path:path>')
 def landing_page(path):
-    return send_from_directory(views_blueprint.static_folder, 'index.html')
+    if path != "" and os.path.exists(views_blueprint.static_folder +
+                                     '/' + path):
+        return send_from_directory(views_blueprint.static_folder, path)
+    else:
+        return send_from_directory(views_blueprint.static_folder, 'index.html')
 
-# @views_blueprint.errorhandler(404)
-# def not_found(e):
-#     return app.send_static_file('index.html')
+@views_blueprint.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
