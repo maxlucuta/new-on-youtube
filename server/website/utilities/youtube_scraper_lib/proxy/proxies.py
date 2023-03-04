b8ac893b562f85574b7d87d288e6beb242882a4f
Maximilian Lucuta <113853857+maxlucuta@users.noreply.github.com>
2023-03-04 14:11:12 +0000

    commented proxy.py run_tests run_deploy

--- server/website/utilities/youtube_scraper_lib/proxy/proxies.py
+++ server/website/utilities/youtube_scraper_lib/proxy/proxies.py
@@ -1,20 +1,33 @@
-from fp.fp import FreeProxy, FreeProxyException
 import os
 import requests
+from fp.fp import FreeProxy, FreeProxyException
 
 
 class Proxy:
+    """Class responsible for generating and configuring proxies."""
+
     def __init__(self, region: list[str], rand: bool, website: str):
+        """Constructs a Proxy objext.
+
+        Args:
+            region (list[str]): list of region codes, eg. GB, US
+            rand (bool): if true allows for random shuffling of proxies
+            website (str): intended website for proxy use
+        """
+
         self.proxy_server = FreeProxy(region, rand=rand)
         self.website = website
         self.proxy = None
         self.host = None
 
     def rotate(self):
+        """Rotates proxies when called, assumed to be called only if
+           previous proxies have been blocked. Wipes previous proxies
+           and attemps to generate new ones.
+        """
 
         self.proxy = None
         self.host = None
-
         while True:
             try:
                 proxy = self.proxy_server.get().split("//")
@@ -30,12 +43,26 @@ class Proxy:
                 break
 
     def get(self) -> dict[str, str]:
+        """Returns most recent proxy generated, if there isn't one
+           it will attempt to generate new proxies and update OS
+           environmental variables.
+
+        Returns:
+            dict[str, str]: http/https proxy in requests format
+        """
+
         if not self.proxy:
             self.rotate()
             self.set_env()
         return self.proxy
 
     def set_env(self):
+        """If a proxy has been generated, it will update OS
+           environmental variables with the proxy so it can be used
+           by external libraries that do not offer request format
+           parsing.
+        """
+
         if self.host:
             request_type = list(self.proxy.keys())[0]
             os.environ[request_type.upper() + "_PROXY"] = self.host
