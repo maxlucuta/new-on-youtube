97c6059d4cc40f74b9e1278b4a191a67ad667db6
Maximilian Lucuta <113853857+maxlucuta@users.noreply.github.com>
2023-03-05 00:06:51 +0000

    run_tests run_deploy

--- server/tests/unit/test_video_database.py
+++ server/tests/unit/test_video_database.py
@@ -5,6 +5,7 @@ def test_db_contains_video_returns_valid_response():
     assert (db.db_contains_video('f1', 'aouCUd_NcmE'))
 
 
+#remove when working
 # def test_get_recommended_videos_returns_valid_response():
 #   assert (db.get_recommended_videos('devuser4', 10))
 
